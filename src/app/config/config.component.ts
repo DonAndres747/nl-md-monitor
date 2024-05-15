c6164e55ecacbfc097dd228fb36c36ad964000d2
Andres Felipe Giraldo Bustos <andres.giraldo@netlogistik.com>
2024-05-15 10:20:30 -0500

    NL-015 terminacion vista configuracion tep junto con manejo updates

--- src/app/config/config.component.ts
+++ src/app/config/config.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, SimpleChanges } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ConfigService } from '../services/config.service';
 import { ClientModel } from '../model/client.model';
@@ -32,6 +32,8 @@ export class ConfigComponent implements OnInit {
   val1: string = '';
   val2: string = '';
 
+  tepIntData: any[] = [];
+
   ngOnInit() {
     this.rutaActiva.paramMap.subscribe((params) => {
       this.solution = params.get('sol')?.toUpperCase();
@@ -41,43 +43,85 @@ export class ConfigComponent implements OnInit {
         .getConnectionsUrls(this.dbName, this.solution.toLowerCase())
         .subscribe((response) => {
           (this.id = response.id),
-            (this.end1 = Object.values(response.url)[0]),
-            (this.end2 = Object.values(response.url)[1]),
-            (this.endName1 = Object.keys(response.url)[0]),
-            (this.endName2 = Object.keys(response.url)[1]),
             (this.service = response.service),
-            (this.login = response.login);
+            (this.login = response.login),
+            this.solution != 'TEP'
+              ? ((this.end1 = Object.values(response.url)[0]),
+                (this.end2 = Object.values(response.url)[1]),
+                (this.endName1 = Object.keys(response.url)[0]),
+                (this.endName2 = Object.keys(response.url)[1]))
+              : Object.entries(response.url).map(([key, value]) => {
+                  this.tepIntData.push({
+                    name: key,
+                    currentValue: value,
+                    newValue: '',
+                  });
+                });
         });
     });
   }
 
   updateConnections() {
-    this.configService
-      .updateConnections(this.solution.toLowerCase(), this.serviceVal, {
+    let dataToSend: any;
+
+    if (this.solution !== 'TEP') {
+      dataToSend = {
         [this.endName1]: this.val1 || this.end1,
         [this.endName2]: this.val2 || this.end2,
-      })
+      };
+    } else {
+      const tepData: any = {};
+      this.tepIntData.forEach((value) => {
+        tepData[value.name] = value.newValue || value.currentValue;
+      });
+      dataToSend = tepData;
+    }
+
+    this.configService
+      .updateConnections(
+        this.solution.toLowerCase(),
+        this.serviceVal,
+        dataToSend
+      )
       .subscribe((response) => {
-        response == 'Success' ? this.refreshRoute() : '';
+        response === 'Success' ? this.refreshRoute() : '';
       });
   }
 
   active() {
-    return !(
-      (this.service != this.serviceVal && this.serviceVal != '') ||
-      (this.end1 != this.val1 && this.val1 != '') ||
-      (this.end2 != this.val2 && this.val2 != '')
-    );
+    if (this.solution != 'TEP') {
+      return !(
+        (this.service != this.serviceVal && this.serviceVal != '') ||
+        (this.end1 != this.val1 && this.val1 != '') ||
+        (this.end2 != this.val2 && this.val2 != '')
+      );
+    } else {
+      return !(
+        (this.service != this.serviceVal && this.serviceVal != '') ||
+        this.tepIntData.filter((data) => {
+          return data.newValue != '';
+        }).length > 0
+      );
+    }
   }
 
   clearinputs() {
     this.val1 = '';
     this.val2 = '';
     this.serviceVal = '';
+    this.solution == 'TEP'
+      ? this.tepIntData.forEach((element) => {
+          element.newValue = '';
+        })
+      : '';
   }
 
   refreshRoute() {
     alert('URL actualizada con exito');
     window.location.reload();
+  }
+
+  capitalizeFirstLetter(str: string): string {
+    return str.replace(/\b\w/g, (char) => char.toUpperCase());
   }
 }
